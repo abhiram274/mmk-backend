fcdc1dc16e96cb8488f2c764d6aaee4c7865a008
VARA-06 <varakosuru@gmail.com>
2025-06-14 21:32:15 +0530

    pls do this

--- utils/sendcertificate.js
+++ utils/sendcertificate.js
@@ -5,26 +5,6 @@ const { PDFDocument, StandardFonts, rgb } = require('pdf-lib');
 const nodemailer = require('nodemailer');
 require("dotenv").config();
 
-// 📄 Helper to create PDF certificate
-// function generateCertificate(name, eventName) {
-//   const fileName = `certificates/${name.replace(/\s/g, "_")}_${Date.now()}.pdf`;
-
-//   if (!fs.existsSync('certificates')) {
-//     fs.mkdirSync('certificates');
-//   }
-
-//   const doc = new PDFDocument();
-//   doc.pipe(fs.createWriteStream(fileName));
-
-//   doc.fontSize(28).text('Certificate of Participation', { align: 'center' });
-//   doc.moveDown();
-//   doc.fontSize(20).text(`This certifies that ${name}`, { align: 'center' });
-//   doc.moveDown();
-//   doc.text(`has successfully participated in "${eventName}"`, { align: 'center' });
-//   doc.end();
-
-//   return fileName;
-// }
 
 async function generateCertificate(name, eventName, description = "") {
   const templatePath = path.join(__dirname, '../../template-certificate.pdf');
